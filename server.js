6a08c74cad4ee4b0d0e8b8ca4da9fdc88bb18c35
Ian Macalinao <me@ian.pw>
2015-02-21 22:50:52 -0600

    yandexxx

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-module.exports = function() {
-};
+var express = require('express');
+var translate = require('yandex-translate');
+
+var app = express();
+
+app.get('/translate', function(req, res) {
+  translate(req.query.text, {
+    from: req.query.from || 'es',
+    to: req.query.to || 'en',
+    key: process.env.YANDEX_KEY
+  }, function(err, t) {
+    res.json({
+      result: (t.text || [])[0]
+    });
+  });
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
